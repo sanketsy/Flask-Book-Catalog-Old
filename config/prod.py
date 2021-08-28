b405daa426907a250940640bf777d2db02a90411
sanketsy <yelkarsanket@gmail.com>
2021-08-28 19:29:07 +0530

    Modified code for Production deployment

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = False
-SECRET_KEY = "topsecret"
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/catalog_db'
+SECRET_KEY = 'bb18c29863bd5c91d87af715'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
