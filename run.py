42aac395c8992ecfacc025083157a59a4e63529e
sanketsy <yelkarsanket@gmail.com>
2021-08-28 19:31:23 +0530

    Removed if main statement

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from app import create_app, db
 
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-    flask_app.run()
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+flask_app.run()
 
